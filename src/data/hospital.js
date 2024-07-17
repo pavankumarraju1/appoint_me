181235cecc492e4bac6a4885038cc85f2a15cb38
sailesh venkata <saileshvissamraju@gmail.com>
2024-07-17 19:27:19 +0530

    data folder added

---
+++ src/data/hospital.js
@@ -0,0 +1,10 @@
+const data=[{"patient_id":1,"appointment_date":"12/12/2022","doctor_name":"Jennie Oxer","rating":5,"medical_condition":"Heart Disease"},
+    {"patient_id":2,"appointment_date":"4/10/2022","doctor_name":"Packston Vaudin","rating":2,"medical_condition":"Heart Disease"},
+    {"patient_id":3,"appointment_date":"8/5/2022","doctor_name":"Valeda Hurd","rating":5,"medical_condition":"Cancer"},
+    {"patient_id":4,"appointment_date":"12/8/2022","doctor_name":"Ernest Clac","rating":3,"medical_condition":"Heart Disease"},
+    {"patient_id":5,"appointment_date":"11/4/2022","doctor_name":"Allissa Brugger","rating":2,"medical_condition":"Cancer"},
+    {"patient_id":6,"appointment_date":"10/21/2022","doctor_name":"Evelin Placide","rating":4,"medical_condition":"Cancer"},
+    {"patient_id":7,"appointment_date":"12/1/2022","doctor_name":"Caesar McGarva","rating":4,"medical_condition":"Cancer"},
+    {"patient_id":8,"appointment_date":"1/30/2022","doctor_name":"Gibby Hughlin","rating":1,"medical_condition":"Diabetes"},
+    {"patient_id":9,"appointment_date":"6/25/2022","doctor_name":"Mick McNirlan","rating":4,"medical_condition":"Cancer"},
+    {"patient_id":10,"appointment_date":"1/23/2022","doctor_name":"Sibella Elsley","rating":4,"medical_condition":"Cancer"}]
